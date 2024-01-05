ba2801c187d165e05b7dfdb3368b51ce586399f0
Muhammad Saim <saim8711@gmail.com>
2024-01-05 15:49:07 +0530

    My test commit

---
+++ MyFPP.py
@@ -0,0 +1,11 @@
+import pandas as pd
+
+# Creating a DataFrame from a dictionary
+data = {'Name': ['Saim', 'Saboor', 'Hamza'],
+        'Age': [35, 23, 22],
+        'City': ['London', 'Lucknow', 'Vadodra']}
+
+df = pd.DataFrame(data)
+
+# Displaying the DataFrame
+print(df)
